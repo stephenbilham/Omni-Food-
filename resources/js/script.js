b1e96937dbf86c8c8e1d029bd1ea070c85f90961
stephen bilham <stephenbilham@yahoo.com>
2020-02-10 16:06:33 -0800

    animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -71,4 +71,38 @@ $(document).ready(function() {
         }
       }
     });
+
+  /* Animations on scroll*/
+  $(".js--wp-1").waypoint(
+    function(direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "55%"
+    }
+  );
+  $(".js--wp-2").waypoint(
+    function(direction) {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "55%"
+    }
+  );
+  $(".js--wp-3").waypoint(
+    function(direction) {
+      $(".js--wp-3").addClass("animated fadeIn ");
+    },
+    {
+      offset: "55%"
+    }
+  );
+  $(".js--wp-4").waypoint(
+    function(direction) {
+      $(".js--wp-4").addClass("animated pulse");
+    },
+    {
+      offset: "55%"
+    }
+  );
 });
